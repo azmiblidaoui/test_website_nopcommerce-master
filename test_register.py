790891df4cb395c8f1daf87f7c2ce545d909fb5c
azmiblidaoui <azmi.blidaoui19@gmail.com>
2023-04-24 17:33:06 +0100

    test register

---
+++ test_register.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+def test_register_with_mandatory_fields():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("https://demo.nopcommerce.com/")
+    driver.find_element(By.XPATH,"//a[@class='ico-register']").click()
+    driver.find_element(By.ID,"FirstName").send_keys("test")
+    driver.find_element(By.ID,"LastName").send_keys("test")
+    driver.find_element(By.ID,"Email").send_keys("tt5820724@gmail.com")
+    driver.find_element(By.ID,"Password").send_keys("azerty@123")
+    driver.find_element(By.ID,"ConfirmPassword").send_keys("azerty@123")
+    driver.find_element(By.ID,"register-button").click()
+    expected_heading_text = "Your registration completed"
+    assert driver.find_element(By.XPATH,"//div[@class='result']").text.__eq__(expected_heading_text)
+
