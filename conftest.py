b6c399e22071641a4b976debbc70c9af303971cd
azmiblidaoui <azmi.blidaoui19@gmail.com>
2023-04-25 14:01:45 +0100

    reading data from config file

--- conftest.py
+++ conftest.py
@@ -1,10 +1,21 @@
 import pytest
 from selenium import webdriver
+from utilities import ReadConfigurations
 @pytest.fixture()
 def setUp_and_teardown(request):
-    driver =webdriver.Chrome()
+    browser = ReadConfigurations.read_configuration("basic info","browser")
+    if browser.__eq__("chrome"):
+        driver =webdriver.Chrome()
+    elif browser.__eq__("firfox"):
+        driver = webdriver.Firefox()
+    elif browser.__eq__("edge"):
+        driver = webdriver.Edge()
+    else:
+        print("add browser name frome 'config file ', can you use ' chrome, firfox, edge '  ")
+
     driver.maximize_window()
-    driver.get("https://demo.nopcommerce.com/")
+    url = ReadConfigurations.read_configuration("basic info","url")
+    driver.get(url)
     request.cls.driver = driver
     yield
     driver.quit()
