43bbd3ce8f96e73c629a8ee4107154c10894cae2
azmiblidaoui <azmi.blidaoui19@gmail.com>
2023-04-25 23:52:09 +0100

    working with POM 'TestLogin'

--- tests/test_Login.py
+++ tests/test_Login.py
@@ -1,11 +1,18 @@
 import pytest
+from pages.HomePage import HomePage
+from pages.LoginPage import LoginPage
+from pages.MyAccount import MyAccount
 from selenium.webdriver.common.by import By
 @pytest.mark.usefixtures("setUp_and_teardown")
 class TestLogin:
     def test_login_with_valid_credentials(self):
-        self.driver.find_element(By.XPATH,"//a[@class='ico-login']").click()
-        self.driver.find_element(By.ID,"Email").send_keys("tt5820724@gmail.com")
-        self.driver.find_element(By.ID,"Password").send_keys("azerty@@123")
-        self.driver.find_element(By.XPATH,"//button[normalize-space()='Log in']").click()
-        assert self.driver.find_element(By.LINK_TEXT,"My account").is_displayed()
-        self.driver.quit()
+        home_page =HomePage(self.driver)
+        home_page.click_login()
+        login_page = LoginPage(self.driver)
+        login_page.enter_email_address("tt5820724@gmail.com")
+        login_page.enter_passowrd("azerty@@123")
+        login_page.click_on_login_button()
+        my_account = MyAccount(self.driver)
+        assert my_account.display_status_of_edit_your_account_information_option()
+        
+        
