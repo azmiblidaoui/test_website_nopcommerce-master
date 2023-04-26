58871f3454b5fadd5e1988929ff204c8b2734e5d
azmiblidaoui <azmi.blidaoui19@gmail.com>
2023-04-26 02:59:50 +0100

    test register page

--- tests/test_register.py
+++ tests/test_register.py
@@ -1,15 +1,23 @@
 import pytest
+import time
+from pages.HomePage import HomePage
+from pages.RegisterPage import RegisterPage
 from selenium.webdriver.common.by import By
 @pytest.mark.usefixtures("setUp_and_teardown")
 class TestRegister:
     def test_register_with_mandatory_fields(self):
-        self.driver.find_element(By.XPATH,"//a[@class='ico-register']").click()
-        self.driver.find_element(By.ID,"FirstName").send_keys("test")
-        self.driver.find_element(By.ID,"LastName").send_keys("test")
-        self.driver.find_element(By.ID,"Email").send_keys("tt5820724@gmail.com")
-        self.driver.find_element(By.ID,"Password").send_keys("azerty@123")
-        self.driver.find_element(By.ID,"ConfirmPassword").send_keys("azerty@123")
-        self.driver.find_element(By.ID,"register-button").click()
+        home_page=HomePage(self.driver)
+        home_page.click_register()
+        register_page = RegisterPage(self.driver)
+    
+        register_page.enter_first_name("test")
+        
+        register_page.enter_last_name("test")
+
+        register_page.enter_email("tt5820724@gmail.com")
+        register_page.enter_password("azerty@123")
+        register_page.enter_confirm_password("azerty@123")
+        register_page.click_register_button()
+
         expected_heading_text = "Your registration completed"
         assert self.driver.find_element(By.XPATH,"//div[@class='result']").text.__eq__(expected_heading_text)
-
